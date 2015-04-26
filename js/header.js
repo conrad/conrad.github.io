a7772153f70ae2bfc0c0a4c2b15083d4e78fb1b0
Matt Conrad <mmconrad@gmail.com>
2015-04-25 22:51:07 -0700

    works on header and removes CNAME file

--- js/header.js
+++ js/header.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   
-  var movementStrength = 1100;
+  var movementStrength = 50;
   var width = movementStrength / $(window).width();
+  var height = movementStrength / $(".site-banner").height();
   $(".site-banner").mousemove(function(e){
     // var pageX = e.pageX - ($(window).width() / 2);
-    var newvalueX = width * e.pageX * -1;
-    $(".site-banner").css("background-position", newvalueX+"px  "+"0px");
+   var newvalueX = width * e.pageX * -1 + (width/1.5);
+   var newvalueY = height * e.pageY * -1 + (height/1.5);
+   console.log(e);
+    $(".site-banner").css("background-position", newvalueX + "px  " + newvalueY + "px");
   });
 
   $(".site-banner").on("click", function() {
